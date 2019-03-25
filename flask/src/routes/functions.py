8ec88c8b28086708c625e09bc0e0c0b7452d26c3
L <kuyahshtibov@gmail.comgit config --global user.email kuyahshtibov@gmail.com>
2019-03-25 22:23:24 +0300

    added some basic functions

--- flask/src/routes/functions.py
+++ flask/src/routes/functions.py
@@ -1,3 +1,14 @@
 from flask import Blueprint
 
 functions = Blueprint('functions', __name__)
+
+@functions.route('/diff', methods='POST')
+def execute_diff(func,**kwargs):
+    return func(**kwargs)
+
+@functions.route('/uni',methods='POST')
+def execute_uni(*args,**kwargs):
+    for i in args:
+        tmp = i(input=kwargs['input'])
+        kwargs['input'] = tmp
+    return tmp
