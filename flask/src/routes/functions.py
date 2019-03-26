879e065f7f74309dfd671fcd52124ff81ec7b2f1
Misgorod <andbulgaru@gmail.com>
2019-03-26 20:05:32 +0300

    Init react

--- flask/src/routes/functions.py
+++ flask/src/routes/functions.py
@@ -2,11 +2,11 @@ from flask import Blueprint
 
 functions = Blueprint('functions', __name__)
 
-@functions.route('/diff', methods='POST')
+@functions.route('/diff', methods=['POST'])
 def execute_diff(func,**kwargs):
     return func(**kwargs)
 
-@functions.route('/uni',methods='POST')
+@functions.route('/uni',methods=['POST'])
 def execute_uni(*args,**kwargs):
     for i in args:
         tmp = i(input=kwargs['input'])
