60e42b256e31fa299c48f7ac684bd3db16ac3070
L <kuyahshtibov@gmail.comgit config --global user.email kuyahshtibov@gmail.com>
2019-03-28 18:44:48 +0300

    get

--- flask/src/routes/data.py
+++ flask/src/routes/data.py
@@ -4,6 +4,7 @@ from flask import Blueprint, make_response, request
 from models import Column, Data
 from utils.parse import parse
 from werkzeug.utils import secure_filename
+from Funcdicts.functions_base import funcdict
 
 data = Blueprint('data', __name__)
 
@@ -32,3 +33,24 @@ def upload():
         data.file.put(req_file)
         data.save()
         return data.json(), 200
+
+@data.route('/<id>', methods=['GET'])
+def preprocess(id):
+    result = []
+    json = request.get_json()
+    columns = json['columns']
+    functions = json['functions']
+    data = list(filter(lambda x: x.name in columns,Data.objects(id=id).columns))
+    for i in data:
+        for j in functions:
+            if isinstance(j,list):
+                for k in j:
+                    i['data'] = funcdict[k['function']['name']]['func'](inp = np.array(i['data']),**k['args'])
+            else :
+                i['data'] = funcdict[k['function']['name']]['func'](inp = np.array(i['data']),**j['args'])
+    return data, 200
+
+
+    
+
+
