7512799d387b9034bbb12119eda931635e9f87d0
DanKhizov <dan.khizov@gmail.com>
2019-03-29 07:54:24 +0300

    add few options of charts

---
+++ react/src/components/Chart/Chart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Chart.scss";
+import { Chart } from "react-google-charts";
+
+const Chart = () => {
+	return (
+		<div>
+			<h1>Chart</h1>
+		</div>
+	);
+};
+
+export default Chart;
