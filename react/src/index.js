1148432bd064b6bf0f29f3f1923c9331485c0362
DanKhizov <dan.khizov@gmail.com>
2019-03-30 21:37:33 +0300

    Remove react-router-dom from project

--- react/src/index.js
+++ react/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import ErrorBoundary from "./components/ErrorBoundary";
@@ -10,9 +9,7 @@ import App from "./components/App";
 ReactDOM.render(
 	<Provider store={store}>
 		<ErrorBoundary>
-			<Router>
-				<App />
-			</Router>
+			<App />
 		</ErrorBoundary>
 	</Provider>,
 	document.getElementById("root")
