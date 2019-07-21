d4b61fa22c5c036f5137b024c5bd11b4e627e1f6
misgorod <andbulgaru@gmail.com>
2019-07-21 20:34:11 +0300

    init

--- flask/src/app.py
+++ flask/src/app.py
@@ -1,3 +1,6 @@
+from routes.tests import tests
+from routes.functions import functions
+from routes.data import data
 import os
 
 from flask import Flask
@@ -5,18 +8,16 @@ from json_encoder import CustomJSONEncoder
 # Configure database
 from mongoengine import connect
 
-connect('datafactoring', host='mongo', port=27017, username='admin', password='admin')
+connect('datafactoring', host='mongo', port=27017,
+        username='admin', password='admin')
 
 # Configure main application
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='static')
 app.config.from_object('config')
 app.secret_key = os.urandom(24)
 app.json_encoder = CustomJSONEncoder
 
 # Register routes
-from routes.data import data
-from routes.functions import functions
-from routes.tests import tests
 
 app.register_blueprint(data, url_prefix='/api/data/')
 app.register_blueprint(functions, url_prefix='/api/functions/')
